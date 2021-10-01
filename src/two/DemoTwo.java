8b9fb9f7e74465691d0e04a5d10252fa91bc6958
hirah <hirah@DESKTOP-M75OOQ6.myfiosgateway.com>
2021-09-30 21:12:07 -0400

    xyz added

--- src/two/DemoTwo.java
+++ src/two/DemoTwo.java
@@ -3,6 +3,12 @@ package two;
 public class DemoTwo {
 	public static void main(String[] args) {
 		System.out.println("Demo Two");
+		boolean isSunday = true;
+		if(isSunday) {
+			System.out.println("abc");
+		}else {
+			System.out.println("Xyx");
+		}
 	}
 	
 
